a9afc9a2b4278adcbc7b28a3cbb9056b9f33577a
Hamish Coleman <hamish@zot.org>
2009-09-09 16:44:11 +1000

    Add action objects and null objects

--- robot.pm
+++ robot.pm
@@ -2,13 +2,111 @@
 # This is a library for automated cities character actions
 #
 #
+use strict;
+use warnings;
+
+
+#
+# Handler for activities touching a null result
+# - click is for null actions
+# - wield is for null items
+#
+# Basically, if you search for a item/action that is not present, you will
+# get an instance of this class instead of an undef
+#
+package Cities::Null;
+
+sub new {
+	my ($class,$id) = @_;
+	my $self = { _id => $id };
+	bless $self, $class;
+	return $self;
+}
+
+sub click {
+	warn "Attempting to click a null action ($self->{_id})";
+	return undef;
+}
+
+sub wield {
+	warn "Attempting to wield a null item ($self->{_id})";
+	return undef;
+}
+
+#
+# An Action is any of the buttons on the screen
+#
+package Cities::Action;
+
+sub new {
+	my ($class,$name,$value) = @_;
+	my $self = {};
+	bless $self, $class;
+
+	if (!$name) {
+		die "Actions must have names";
+	}
+	$self->name($name);
+	$self->value($value);
+
+	return $self;
+}
+
+sub container {
+	my ($self,$v) = @_;
+
+	$v && ($self->{_container} = $v);
+	return $self->{_container};
+}
+
+sub name {
+	my ($self,$v) = @_;
+
+	$v && ($self->{_name} = $v);
+	return $self->{_name};
+}
+
+sub ap {
+	my ($self,$v) = @_;
+
+	$v && ($self->{_ap} = $v);
+	return $self->{_ap};
+}
+
+sub value {
+	my ($self,$v) = @_;
+
+	$v && ($self->{_value} = $v);
+
+	if ($v && $v =~ m/ \((\d+) AP\)$/) {
+		$self->ap($1);
+	}
+
+	return $self->{_value};
+}
+
+sub click {
+	my ($self,$force) = @_;
+
+	if (!$self->container) {
+		die "Must have a container to click";
+	}
+
+	if (!$force
+	    && $self->{_ap}
+	    && ($self->{_ap} +10) > $self->container->ap) {
+		# not enough AP to click
+		# NOTE: hardcoded guard of 10 spare AP
+		return undef;
+	}
+
+	return $self->container->_click($self->name);
+}
 
 #
 # An Item that can be in your inventory
 #
 package Cities::Item;
-use strict;
-use warnings;
 
 sub new {
 	my ($class,$value,$name) = @_;
@@ -72,11 +170,11 @@ sub container {
 sub wield {
 	my ($self) = @_;
 
-	if (!$self->{_container}) {
+	if (!$self->container) {
 		die "Cannot wield an item with no container"
 	}
 
-	return $self->{_container}->_wield($self);
+	return $self->container->_wield($self);
 }
 
 sub avgdamage {
@@ -93,9 +191,6 @@ sub avgdamage {
 # The main robot system
 #
 package Robot;
-use strict;
-use warnings;
-
 
 #
 # Libs we need.
@@ -167,6 +262,23 @@ sub populate_items_list {
 	1;
 }
 
+sub populate_actions_list {
+	my ($self) = @_;
+
+	if (!$self->{_form}) {
+		die "No form data";
+	}
+
+	for ($self->{_form}->find_input) {
+		next if (!$_->name);
+		my $action = Cities::Action->new($_->name,$_->value);
+		$action->container($self);
+
+		$self->{_actions}{$_->{name}} = $action;
+	}
+	1;
+}
+
 sub populate_scrape_data {
 	my ($self) = @_;
 
@@ -204,6 +316,7 @@ sub populate_scrape_data {
 	dumptextintodb($self->{_d});
 
 	$self->populate_items_list;
+	$self->populate_actions_list;
 
 	return 1;
 }
@@ -350,9 +463,46 @@ sub item {
 	}
 
 	if ($item) {
-		return $self->{_items}{$item};
+		my $item = $self->{_items}{$item};
+		if ($item) {
+			return $item;
+		}
+		return Cities::Null->new;
 	}
 	return values %{$self->{_items}};
+}
+
+sub _click {
+	my ($self,$action) = @_;
+
+	if (!$self->{_form}) {
+		$self->request;
+		$self->{_form} || die "No form data";
+	}
+
+	if (!$action) {
+		die "Cannot action a null value"
+	}
+
+	$self->request($self->{_form}->click($action->name));
+}
+
+sub action {
+	my ($self,$action) = @_;
+
+	if (!$self->{_actions}) {
+		$self->request;
+		$self->{_actions} || die "No items";
+	}
+
+	if ($action) {
+		my $action = $self->{_actions}{$action};
+		if ($action) {
+			return $action;
+		}
+		return Cities::Null->new;
+	}
+	return values %{$self->{_actions}};
 }
 
 sub rxy {
@@ -364,6 +514,17 @@ sub rxy {
 	}
 
 	return ($self->{_d}{_realm},$self->{_d}{_x},$self->{_d}{_y});
+}
+
+sub ap {
+	my ($self) = @_;
+
+	if (!$self->{_d}) {
+		$self->request;
+		$self->{_d} || die "No scrape data";
+	}
+
+	return ($self->{_d}{ap});
 }
 
 1;
@@ -379,6 +540,13 @@ my $r = Robot->new('_LOGNAME_','_PASSWORD_');
 $r->login;
 print "Robot is at ",join('/',$r->rxy),"\n";
 $r->item('CruelBlade')->wield;
+
+# If possible, use a monastry
+$r->action('act_retreat')->click;
+
+# If possible, use the cornucopia
+$r->action('act_item_eat')->click;
+$r->action('act_item_drink')->click;
 
 $r->monster - returns a direction object list
 $r
